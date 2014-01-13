c9c1350baaa8b5c5920d16c67dffd52be1b0ad46
Jeffrey Wilcke <stygeo@gmail.com>
2014-01-13 16:16:42 +0100

    Initial

---
+++ mastercoin.go
@@ -0,0 +1,61 @@
+package mscd
+
+import (
+  "github.com/conformal/btcutil"
+  _"github.com/conformal/btcwire"
+  _"github.com/conformal/btcscript"
+  "github.com/mastercoin-MSC/mscutil"
+  "log"
+  _"errors"
+)
+
+var MastercoinBlockChannel chan *btcutil.Block
+func QueueMessageForBlockChannel(block *btcutil.Block) {
+  go func() {
+    MastercoinBlockChannel <- block
+  }()
+}
+
+type MastercoinServer struct {
+  quit       chan bool
+}
+
+func NewMastercoinServer() *MastercoinServer {
+  MastercoinBlockChannel = make(chan *btcutil.Block, 1)
+
+  return &MastercoinServer{}
+}
+
+func (s *MastercoinServer) ProcessBlock(block *btcutil.Block) error {
+  mscutil.GetExodusTransactions(block)
+
+  return nil
+}
+
+func (s *MastercoinServer) Stop() {
+  s.quit <- true
+}
+
+// Mastercoin main loop. Takes care of listening on the maistercoin block channel
+// and calls the appropriate methods
+func MastercoinMain() {
+  // Main server instance
+  server := NewMastercoinServer()
+out:
+  for {
+    select {
+    case block := <- MastercoinBlockChannel:
+      if block.Height() == 249497 {
+        log.Panic("We got it bitch")
+      }
+
+      //err := server.ProcessBlock(block)
+      //if err != nil {
+      //  log.Println(err)
+      //  continue
+      //}
+    case <- server.quit:
+      break out
+    }
+  }
+}
