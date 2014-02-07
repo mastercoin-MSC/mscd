2765022b880279483fc9eae9afd20d8d17ab5876
Jeffrey & Maran <dev@go>
2014-02-07 10:04:02 -0500

    Fundraiser/Playback/Simple send implementations
    Changed Fundraiser creation from new transactions.
    
    Fixed ClassB transactions.
    
    Don't depend on the error for checking DX/Fundraiser messages.
    
    Added the ability to parse multiple messages (ClassA + Fund)

--- mastercoin.go
+++ mastercoin.go
@@ -3,13 +3,16 @@ package mscd
 import (
 	_ "errors"
 	"github.com/conformal/btcdb"
+	_"log"
+	"math"
 	_ "github.com/conformal/btcscript"
 	"github.com/conformal/btcutil"
 	_ "github.com/conformal/btcwire"
 	"github.com/mastercoin-MSC/mscutil"
-	"fmt"
-	"bytes"
-	_ "encoding/gob"
+	_"fmt"
+	"os"
+	"os/user"
+	"path"
 )
 
 const (
@@ -39,99 +42,71 @@ type MastercoinServer struct {
 	msgParser *MsgParser
 
 	btcdb btcdb.Db
+
+	db *mscutil.LDBDatabase
+
+	FundraiserHandler *FundraiserHandler
+	SimpleSendHandler *SimpleSendHandler
 }
 
 func NewMastercoinServer(btcdb btcdb.Db) *MastercoinServer {
 	mscutil.Logger.Println("Creating mastercoin server")
 	MastercoinBlockChannel = make(chan *btcutil.Block, blockChannelQueueLength)
 
+	// TODO: Rename this to replay database once we start reimporting
+	db, err := mscutil.NewLDBDatabase("db")
+	bDb, err := mscutil.NewLDBDatabase("bdb")
+	if err != nil {
+		panic("Could not create database")
+	}
+
 	s := &MastercoinServer{
 		quit:         make(chan bool),
 		shutdownChan: make(chan bool),
 		btcdb:        btcdb,
+		db: db,
+		FundraiserHandler: &FundraiserHandler{db: bDb},
+		SimpleSendHandler: &SimpleSendHandler{db: bDb},
 	}
-	parser := NewMsgParser(s)
+	parser := NewMsgParser(s.btcdb, s)
 	s.msgParser = parser
 
 	return s
 }
 
-type TxPack struct {
-	time   int64
-	height int64
-	txs    []*btcutil.Tx
-}
-
-func (pack *TxPack) MarshalBinary() ([]byte, error) {
-	buff := new(bytes.Buffer)
-
-	// Length of the transactions slice so the unmarshaler can allocate a large enough slice
-	length := len(pack.txs)
-	fmt.Fprintln(buff, pack.time, pack.height, length)
-	// Write each tx
-	for _, tx := range pack.txs {
-		var data bytes.Buffer
-		tx.MsgTx().Serialize(&data)
-		buff.Write(data.Bytes())
-
-		fmt.Fprintf(buff, "\n")
-	}
-
-	return buff.Bytes(), nil
-}
-
-func (pack *TxPack) UnmarshalBinary(data []byte) error {
-	buff := bytes.NewBuffer(data)
-
-	var length int
-	_, err := fmt.Fscanln(buff, &pack.time, &pack.height, &length)
-
-	pack.txs = make([]*btcutil.Tx, length)
-	for i := 0; i < length; i++ {
-		var b []byte
-		_, err = fmt.Fscanln(buff, &b)
-		if err == nil {
-			pack.txs[i], err = btcutil.NewTxFromBytes(b)
-			fmt.Println("err from btc")
-		}
-	}
-
-	return err
-}
 
 // Process block takes care of sorting out transactions with exodus output.
 // At this point it doesn't matter whether the Txs are valid or not. Validation
 // is done at the proper handlers.
 func (s *MastercoinServer) ProcessBlock(block *btcutil.Block) error {
+
 	// Gather transactions from this block which had an exodus output
-	txPack := &TxPack{
-		txs:    mscutil.GetExodusTransactions(block),
-		time:   block.MsgBlock().Header.Timestamp.Unix(),
-		height: block.Height(),
+	txPack := &mscutil.TxPack{
+		Txs:    mscutil.GetExodusTransactions(block),
+		Time:   block.MsgBlock().Header.Timestamp.Unix(),
+		Height: block.Height(),
 	}
-	if len(txPack.txs) > 0 {
 
-		/*
-		TODO: We want to start persisting raw data here
-		var buffer bytes.Buffer
-		enc := gob.NewEncoder(&buffer)
-		err := enc.Encode(txPack)
+	// Update Exodus Vesting
+	//
+
+
+	// balance = ((1-(0.5 ** time_difference)) * 5631623576222 .round(8)
+
+
+	if len(txPack.Txs) > 0 {
+		serializedData, err := txPack.Serialize()
 		if err != nil {
-			mscutil.Logger.Fatal(err)
+			return err
 		}
 
-		dec := gob.NewDecoder(&buffer)
-		var pack TxPack
-		err = dec.Decode(&pack)
-		if err != nil {
-			mscutil.Logger.Fatal(err)
-		}
-		*/
+		s.db.CreateTxPack(txPack.Height, serializedData)
+
+		mscutil.Logger.Println("Mastercoin data found at block with height:", txPack.Height)
 
 		// Queue the slice of transactions for further processing by the
 		// message parser.
 		_ = s.msgParser.ProcessTransactions(txPack)
-		// mscutil.Logger.Println(messages)
 	}
 
 	return nil
@@ -144,18 +119,10 @@ out:
 	for {
 		select {
 		case block := <-MastercoinBlockChannel:
-			if block.Height() > ExodusBlockHeight {
-				// TODO debugging hook for exodus
-
-			} else {
-				// Block lower than exodus don't need parsing
-				break
-			}
-
 			// Process the current block, check for Msc Txs
 			err := s.ProcessBlock(block)
 			if err != nil {
-				mscutil.Logger.Println(err)
+				mscutil.Logger.Println("Error processing block:", err.Error())
 				continue
 			}
 		case <-s.quit:
@@ -179,21 +146,69 @@ func (s *MastercoinServer) Stop() {
 	// Close the channel and invoking the channel
 	close(s.quit)
 
+	s.db.Close()
+
 	mscutil.Logger.Println("Stopped successfull")
 }
 
-func (s *MastercoinServer) Start() {
+func GetPath(p string) string {
+	usr, _ := user.Current()
+
+	return path.Join(usr.HomeDir, ".mastercoin", p)
+}
+
+func (s *MastercoinServer) Start(playback bool) {
 	mscutil.Logger.Println("Started mastercoin server")
 	// Start the message parser before the block handler input handler
 	s.msgParser.Start()
 
-	// Start the block inbound processing handler
-	go s.inboundBlockHandler()
+	if playback{
+		mscutil.Logger.Println("Starting playback")
+		mscutil.Logger.Println("Removing database to start fresh")
+
+		path := GetPath("bdb")
+		err := os.RemoveAll(path)
+		if err != nil {
+			mscutil.Logger.Fatal(err)
+		}
+
+
+		iter := s.db.GetDb().NewIterator(nil)
+		for iter.Next() {
+			// Remember that the contents of the returned slice should not be modified, and
+			// only valid until the next call to Next.
+			value := iter.Value()
+
+			newPack := &mscutil.TxPack{}
+			newPack.Deserialize(value)
+
+			mscutil.Logger.Println("\n####### Processing block with height:", newPack.Height, "#############")
+
+			_ = s.msgParser.ProcessTransactions(newPack)
+		}
+		iter.Release()
+
+		// This is where we compare fundraiser consensus
+//		success := TestBalances(s.FundraiserHandler.db)
+
+	}else{
+
+		// Start the block inbound processing handler
+		go s.inboundBlockHandler()
+	}
 
 }
 
 func (s *MastercoinServer) WaitForShutdown() {
 	<-s.shutdownChan
+}
+
+// TODO: When Simple Sends are all synced up; check rounding balance here
+func CalculateMscDevCoins(time int64) float64 {
+	timeDiff := float64(time - 1377993874) / 31556926
+	powr := math.Pow(0.5, timeDiff)
+
+	return (1 - powr ) * 56316.23576222
 }
 
 // Mastercoin main loop. Takes care of listening on the maistercoin block channel
@@ -202,7 +217,5 @@ func MastercoinMain(btcdb btcdb.Db) {
 	// Main server instance
 	server := NewMastercoinServer(btcdb)
 
-	server.Start()
-
-	server.WaitForShutdown()
+	server.Start(false)
 }
--- message_parser.go
+++ message_parser.go
@@ -2,10 +2,18 @@ package mscd
 
 import (
 	"github.com/conformal/btcscript"
+	"github.com/conformal/btcdb"
+	"code.google.com/p/godec/dec"
+	"strings"
 	"github.com/conformal/btcutil"
+	"math/big"
 	"github.com/mastercoin-MSC/mscutil"
+	"encoding/hex"
 	"time"
+	"fmt"
+	"log"
 	"bytes"
+_	"strconv"
 )
 
 // Bitcoin specific type checking
@@ -45,10 +53,12 @@ const (
 )
 
 type MsgParser struct {
-	// Server
 	server *MastercoinServer
+
+	// DB
+	btcdb btcdb.Db
 	// Inbound message channel
-	messageChan chan *TxPack
+	messageChan chan *mscutil.TxPack
 	// Quit indicator
 	quit chan bool
 
@@ -56,12 +66,13 @@ type MsgParser struct {
 	outboundMsgChan chan *Msg
 }
 
-func NewMsgParser(server *MastercoinServer) *MsgParser {
+func NewMsgParser(btcdb btcdb.Db, server *MastercoinServer) *MsgParser {
 	msgParser := &MsgParser{
-		server:          server,
-		messageChan:     make(chan *TxPack, msgChannelSize),
+		messageChan:     make(chan *mscutil.TxPack, msgChannelSize),
 		quit:            make(chan bool),
 		outboundMsgChan: make(chan *Msg, msgChannelSize),
+		btcdb: btcdb,
+		server: server,
 	}
 
 	return msgParser
@@ -72,103 +83,163 @@ func NewMsgParser(server *MastercoinServer) *MsgParser {
 // the proper object which. After a full Msg has been assembled it will return the Msg
 // or an error with a description. Whoever called this function should delegate the msg
 // to the proper object handler.
-func (mp *MsgParser) ParseTx(tx *btcutil.Tx, height, time int64) (msg *Msg, err error) {
-	sender,_ := mscutil.FindSender(tx.MsgTx().TxIn, mp.server.btcdb)
+func (mp *MsgParser) ParseTx(tx *btcutil.Tx, height, time int64) (msgs []*Msg, err error) {
+	sender,_ := mscutil.FindSender(tx.MsgTx().TxIn, mp.btcdb)
 
 	// Check if this is a Class A transaction
 	if isClassA(tx) {
 		mscutil.Logger.Println("Got 'Class A' transaction with hash", tx.Sha())
 
-		highestAddress, _ := mscutil.FindSender(tx.MsgTx().TxIn, mp.server.btcdb)
-
 		// Create a simple send transaction
-		simpleSend, e := mscutil.MakeClassASimpleSend(highestAddress, mscutil.GetAddrsClassA(tx))
+		simpleSend, e := mscutil.MakeClassASimpleSend(sender, mscutil.GetAddrsClassA(tx))
 		err = e
 
 		if simpleSend != nil {
 			mscutil.Logger.Println("Decoded Simple Send transaction:", simpleSend, simpleSend.Data)
-			msg = &Msg{msg: simpleSend}
-			mscutil.Logger.Fatal("SHUTDOWN")
+			msgs = append(msgs, &Msg{msg: simpleSend})
 		}
 	} else {
-		mscutil.Logger.Println("Got 'Class B' tx")
+		mscutil.Logger.Println("Got 'Class B' tx with hash", tx.Sha())
 
 		// Parse addresses from the tx using class B rules
-		plainTextKeys, receiver, err := mscutil.GetAddrsClassB(tx, sender)
+		plainTextKeys, receiver, e := mscutil.GetAddrsClassB(tx, sender)
+		err = e
 		if err == nil {
 			// Receiver is first, data is second in the slice
-			data := plainTextKeys[0][1]
-			// Figure out the message type
-			msgType := mscutil.GetTypeFromAddress(string(data))
-			switch msgType {
+
+			// Let's change 00000014h to 22d
+			data := plainTextKeys[0][2:10]
+			f,_ := hex.DecodeString(data)
+			messageType := int(f[3])
+
+			switch messageType{
 			case mscutil.TxMsgTy:
-				simpleSend, e := mscutil.MakeClassBSimpleSend(plainTextKeys, receiver)
+				simpleSend, e := mscutil.MakeClassBSimpleSend(plainTextKeys, receiver, sender)
 				err = e
 
 				if simpleSend != nil {
-					mscutil.Logger.Println("Got simple send class b transaction")
-					msg = &Msg{msg: simpleSend}
+					// TODO: Do we want to add the sender and receiver to the simple send message?
+					mscutil.Logger.Println("Got Simple Send Class B from",sender,"to",receiver)
+					msgs = append(msgs, &Msg{msg: simpleSend})
 				}
-			case mscutil.DexMsgTy:
 			default:
-				mscutil.Logger.Println("Unknown message type %d. FIXME or erroneus.\n", int(msgType))
+				err = fmt.Errorf("Unknown message type %d. Support for transaction type not implemented yet.\n",messageType)
+				return nil, err
 			}
 		}
 	}
 
-	// If nothing has been found it is either a Exodus Kickstarter / Payment for accept DEx message.
-	if msg == nil && err != nil {
+	if height <= mscutil.FundraiserEndBlock {
+		// Collect the addresses and values for every input used for this transaction
+		highestAddress, _ := mscutil.FindSender(tx.MsgTx().TxIn, mp.btcdb)
 
-		if height <= mscutil.FundraiserEndBlock {
-			// Collect the addresses and values for every input used for this transaction
-			highestAddress, _ := mscutil.FindSender(tx.MsgTx().TxIn, mp.server.btcdb)
-
-			var totalSpend int64
-			for _, txOut := range tx.MsgTx().TxOut {
-				addr, _ := mscutil.GetAddrs(txOut.PkScript)
-				if addr[0].Addr == mscutil.ExodusAddress {
-					totalSpend += txOut.Value
-				}
+		var totalSpend int64
+		for _, txOut := range tx.MsgTx().TxOut {
+			addr, _ := mscutil.GetAddrs(txOut.PkScript)
+			if addr[0].Addr == mscutil.ExodusAddress {
+				totalSpend += txOut.Value
 			}
-
-			fundraiserTx, e := mscutil.NewFundraiserTransaction(highestAddress, totalSpend, time)
-			err = e
-
-			msg = &Msg{msg: fundraiserTx}
-		} else {
-			//MakeDexMessage(outputs)
 		}
+
+		fundraiserTx, e := mscutil.NewFundraiserTransaction(highestAddress, totalSpend, time)
+		err = e
+
+		msgs = append(msgs, &Msg{msg: fundraiserTx})
+	}
+
+	// If nothing has been found it check for Payment for accept DEx message.
+	if len(msgs) == 0 {
+		// TODO:
 	}
 
 	return
 }
 
-func (mp *MsgParser) ProcessTransactions(txPack *TxPack) []*Msg {
-	messages := make([]*Msg, len(txPack.txs))
+type SimpleSendHandler struct {
+	db *mscutil.LDBDatabase
+}
 
-	for _, tx := range txPack.txs {
+type FundraiserHandler struct {
+	db *mscutil.LDBDatabase
+}
+
+func (frh *FundraiserHandler) Process(msg *Msg, err error){
+	pack := msg.msg.(*mscutil.FundraiserTransaction)
+
+	// Get the current balance sheet map, where the key is the currency id and the value the amount
+	sheet := frh.db.GetAccount(pack.Addr)
+
+	
+	str := strings.Split(dec.NewDecInt64(0).Mul(pack.Value, dec.NewDecInt64(1e8)).String(), ".")[0]
+	res, _ := big.NewInt(0).SetString(str, 10)
+	dec := res.Uint64()
+
+	sheet[1] += dec
+	sheet[2] += dec
+
+	// Save the key back to the db
+	frh.db.PutAccount(pack.Addr, sheet)
+	sheet = frh.db.GetAccount(pack.Addr)
+}
+func (ssh *SimpleSendHandler) Process(msg *Msg, err error) {
+	pack := msg.msg.(*mscutil.SimpleTransaction)
+
+	sSheet := ssh.db.GetAccount(pack.Sender)
+	rSheet := ssh.db.GetAccount(pack.Receiver)
+	id := pack.Data.CurrencyId
+
+	if sSheet[id] >= pack.Data.Amount{
+		// Valid Transaction
+	    rSheet[id] += pack.Data.Amount
+	    sSheet[id] -= pack.Data.Amount
+
+	    ssh.db.PutAccount(pack.Sender, sSheet)
+	    ssh.db.PutAccount(pack.Receiver, rSheet)
+
+	      mscutil.Logger.Println("Send", pack.Data.Amount, "From", pack.Sender, "To", pack.Receiver)
+	} else{
+	      // Invalid Transactions
+	      mscutil.Logger.Println("Not enough balance, transaction invalid")
+	}
+
+
+}
+
+func (mp *MsgParser) ProcessTransactions(txPack *mscutil.TxPack) []*Msg {
+
+	//TODO: Build a way to calculate the difference of MSC vested between this block and last and update the balance accordingly
+	//deVcoins := CalculateMscDevCoins(txPack.Time)
+
+	messages := make([]*Msg, len(txPack.Txs))
+
+	for _, tx := range txPack.Txs {
 		buff := new(bytes.Buffer)
 		tx.MsgTx().Serialize(buff)
 
-		// TODO check if the txs exists in db otherwise skip (it might have happened that only a part of the block has been processed due to power failure)
-		msg, err := mp.ParseTx(tx, txPack.height, txPack.time)
-		if err != nil {
-			mscutil.Logger.Println(err)
-		}
+		msgs, err := mp.ParseTx(tx, txPack.Height, txPack.Time)
 
-		switch msg.msg.(type) {
-		case mscutil.SimpleTransaction:
-			// Process the simple send transaction (simple send transaction handler?)
-			//mp.server.SimpleSendHandler.Process(msg.msg, err)
-		case mscutil.FundraiserTransaction:
-			//mp.server.FundraiserHandler.Process(msg.msg, err)
+		for _, msg := range msgs {
+			if err != nil {
+				mscutil.Logger.Println("Message could not be parsed:", err)
+			}else{
+				switch msg.msg.(type) {
+				case *mscutil.SimpleTransaction:
+					// Process the simple send transaction (simple send transaction handler?)
+					//mp.server.SimpleSendHandler.Process(msg, err)
+				case *mscutil.FundraiserTransaction:
+					// TODO: UNCOMMENT ME
+					mp.server.FundraiserHandler.Process(msg, err)
+				default:
+					log.Fatal("Unknown message type", msg.msg)
+				}
+			}
 		}
 	}
 
 	return messages
 }
 
-func (mp *MsgParser) QueueTransactions(txPack *TxPack) {
+func (mp *MsgParser) QueueTransactions(txPack *mscutil.TxPack) {
 	mp.messageChan <- txPack
 }
 
